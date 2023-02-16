d0d45b994db574f6b4ea779487c2c0f8b0410444
Dima <skaizy44@gmail.com>
2023-02-16 13:41:26 +0200

    hyh
    yhyh

---
+++ src/Components/Crafted/Crafted.js
@@ -0,0 +1,44 @@
+import "../Crafted/Crafted.scss"
+
+function Crafted() {
+    return(
+        <div className="wrapper">
+            <div className="crafted">
+                <div className="crafted__info">
+                    <h2>Crafted with excellent material.</h2>
+                    <p>Donec mattis porta eros, aliquet finibus risus interdum at. Nulla vivethe as it was for us to know what was to be done.</p>
+                    <button className="header__btn crafted__btn">Explore</button>
+                </div>
+                <div className="crafted__doods">
+
+                    <div className="crafted__doods--item">
+                        <img src="./img/crafted-1.png" alt="nordic" />
+                        <div>
+                            <div className="crafted__doods--item__title">Nordic CHAIR</div>
+                            <div className="crafted__doods--item__price">$50.00</div>
+                        </div>
+                    </div>
+
+                    <div className="crafted__doods--item">
+                        <img src="./img/crafted-2.png" alt="Kruzo Aero Chair" />
+                        <div>
+                            <div className="crafted__doods--item__title">Kruzo Aero Chair</div>
+                            <div className="crafted__doods--item__price">$78.00</div>
+                        </div>
+                    </div>
+
+                    <div className="crafted__doods--item">
+                        <img src="./img/crafted-3.png" alt="Ergonomic Chair" />
+                        <div>
+                            <div className="crafted__doods--item__title">Ergonomic Chair</div>
+                            <div className="crafted__doods--item__price">$43.00</div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Crafted;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './normalize.scss';
 import Header from "./Components/Header/Header"
+import Crafted from './Components/Crafted/Crafted';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <Crafted />
   </React.StrictMode>
 );
 
